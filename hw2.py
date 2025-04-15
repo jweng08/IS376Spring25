c9ba77d639b787cf680078f0b93d98d848fb347f
jweng08 <jiajieweng822@gmail.com>
2025-04-14 23:07:01 -0500

    Update hw2.py

--- hw2.py
+++ hw2.py
@@ -1,3 +1,4 @@
+#API for job posting list object
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -22,3 +23,34 @@ async def del_string(job_posting: str = ""):
 @app.get("/job-posting")
 async def get_string():
     return {"Job Posting List": job_postings_list}
+
+#API for resume list object
+from fastapi import FastAPI
+
+app = FastAPI()
+
+resume_list = []
+
+@app.get("/")
+async def root():
+    return {"message": "Hello, please enter a valid endpoint: /resume"}
+
+@app.post("/resume")
+async def add_string(resume: str = ""):
+    resume_list.append(resume)
+    return {"We have a new resume"}
+
+@app.delete("/resume")
+async def del_string(resume: str = ""):
+    if len(resume_list > 0):
+        resume_list.pop(-1)
+    return {"Resume List: ": resume_list}
+
+
+@app.get("/resume")
+async def get_string():
+    return {"Resume List: ": resume_list}
+
+
+
+#API for resume list object
