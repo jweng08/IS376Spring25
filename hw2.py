56f2972795b396dc14dc6525c70737a9601f03aa
jweng08 <jiajieweng822@gmail.com>
2025-04-18 19:50:14 -0500

    Update hw2.py

--- hw2.py
+++ hw2.py
@@ -1,58 +1,34 @@
-#API for job posting list object
-from fastapi import FastAPI
-
-app = FastAPI()
-
-job_postings_list = []
-
-@app.get("/")
-async def root():
-    return {"message": "Hello, please enter a valid endpoint: /job-posting"}
-
-@app.post("/job-posting")
-async def add_string(job_posting: str = ""):
-    job_postings_list.append(job_posting)
-    return {"A new job posting just added": job_posting}
-
-@app.delete("/job-posting")
-async def del_string(job_posting: str = ""):
-    if len(job_postings_list > 0):
-        job_postings_list.pop(-1)
-    return {"Job Posting List: ": job_postings_list}
-
-@app.get("/job-posting")
-async def get_string():
-    return {"Job Posting List": job_postings_list}
-
-#API for resume list object
 from fastapi import FastAPI
 
 app = FastAPI()
 
 resume_list = []
+optimized_resume_list = []
+job_postings_list = []
 
 @app.get("/")
 async def root():
-    return {"message": "Hello, please enter a valid endpoint: /resume"}
+    return {"message": "Hello, please enter a valid endpoint: /resume, /optimized-resume, or /job-posting"}
+
+# Original resume requests
 
 @app.post("/resume")
-async def add_string(resume: str = ""):
+async def add_resume(resume: str = ""):
     resume_list.append(resume)
-    return {"We have a new resume"}
+    return {"We have a new resume: ": resume}
 
 @app.delete("/resume")
-async def del_string(resume: str = ""):
-    if len(resume_list > 0):
+async def del_resume(resume: str = ""):
+    if len(resume_list) > 0:
         resume_list.pop(-1)
     return {"Resume List: ": resume_list}
 
-
 @app.get("/resume")
-async def get_string():
+async def get_resume():
     return {"Resume List: ": resume_list}
 
 
-#API for resume list object
+# Optimized resume requests
 @app.post("/optimized-resume")
 async def add_optimized_resume(optimized_resume: str = ""):
     optimized_resume_list.append(optimized_resume)
@@ -67,3 +43,19 @@ async def del_optimized_resume(optimized_resume: str = ""):
 @app.get("/optimized-resume")
 async def get_optimized_resume():
     return {"Optimized resumes: ": optimized_resume_list}
+
+# Job posting requests
+@app.post("/job-posting")
+async def add_string(job_posting: str = ""):
+    job_postings_list.append(job_posting)
+    return {"A new job posting was added": job_posting}
+
+@app.delete("/job-posting")
+async def del_string(job_posting: str = ""):
+    if len(job_postings_list > 0):
+        job_postings_list.pop(-1)
+    return {"Job Posting List: ": job_postings_list}
+
+@app.get("/job-posting")
+async def get_string():
+    return {"Job Posting List": job_postings_list}
